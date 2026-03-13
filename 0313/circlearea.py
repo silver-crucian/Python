63446d998c4a8a70fa9c1571e254c8a3db021a32
silver-crucian <133368280+silver-crucian@users.noreply.github.com>
2026-03-13 10:24:56 +0900

    Add files via upload

--- 0313/circlearea.py
+++ 0313/circlearea.py
@@ -1,3 +1,10 @@
-radius = 12
-area = 3.14 * radius ** 2
-print("The area of the circle is:", area)
+import math
+
+# 반지름 입력
+r = float(input("반지름을 입력하세요: "))
+
+# 면적 계산
+area = math.pi * r ** 2
+
+# 결과 출력
+print(f"원의 면적: {area}")
